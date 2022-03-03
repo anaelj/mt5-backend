22e86020c681be81ee2784847e415510725a9f4b
Anael Medeiros <anaelj@gmail.com>
2022-03-03 15:46:04 -0400

    add cors

--- server.ts
+++ server.ts
@@ -3,17 +3,20 @@ const app = express();
 import http from 'http';
 const server = http.createServer(app);
 import axios from 'axios';
+import cors from 'cors';
+
+app.use(cors());
 
 const api = axios.create({
   baseURL: 'http://localhost:6542/',
   timeout: 1000,
-  headers: {'Authorization': 'anael'}
+  headers: { 'Authorization': 'anael' }
 });
 
 app.get('/symbols/:symbol', async (req, res) => {
-  console.log( req.params.symbol.toUpperCase());
-  const {data} = await api.get(`symbols/${req.params.symbol.toUpperCase()}`);
-  return  res.send(data);
+  // console.log( req.params.symbol.toUpperCase());
+  const { data } = await api.get(`symbols/${req.params.symbol.toUpperCase()}`);
+  return res.send(data);
 });
 
 server.listen(3000, () => {
